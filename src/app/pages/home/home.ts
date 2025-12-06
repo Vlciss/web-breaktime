9ef8a059bf3ebfc5cbba54002f4a0c54162dadc2
Adrian Vlčko <adrianvlcko@MBPuzivlaAdrian.home>
2025-12-06 03:40:41 +0100

    import CommonModule

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
